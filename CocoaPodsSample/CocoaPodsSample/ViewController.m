9e711ffa6fc613e5eeda7349c1b3d44165427900
redbug <l314kimo@gmail.com>
2012-10-04 18:57:57 +0800

    testing

--- CocoaPodsSample/CocoaPodsSample/ViewController.m
+++ CocoaPodsSample/CocoaPodsSample/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "SVProgressHUD.h"
+
 
 @interface ViewController ()
 
@@ -17,6 +19,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [SVProgressHUD dismissWithSuccess:@"Fuck"];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
