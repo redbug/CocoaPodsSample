25e545db20805f322083a15d9e406bc61d57ad4c
redbug <l314kimo@gmail.com>
2012-10-04 18:44:18 +0800

    init project

---
+++ CocoaPodsSample/CocoaPodsSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CocoaPodsSample
+//
+//  Created by redbug on 10/4/12.
+//  Copyright (c) 2012 redbug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
